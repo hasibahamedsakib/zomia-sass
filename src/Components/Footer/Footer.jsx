3b5d2af32746f5e8c8f3d8ec3fe9380aa122fd56
Hasib Ahamed Sakib <hasibahamedsakib1@gmail.com>
2023-09-09 12:30:30 +0600

    footer form part is compleate

--- src/Components/Footer/Footer.jsx
+++ src/Components/Footer/Footer.jsx
@@ -1,8 +1,12 @@
+import FooterForm from "./FooterForm";
+
 const Footer = () => {
   return (
-    <div>
-      <h1 className="text-4xl text center font bold">Footer is coming...!!!</h1>
-    </div>
+    <footer className="bg-[url('/photo/footer-img.png')] bg-no-repeat py-20 lg:pt-[165px] lg:pb-[25px] bg-cover">
+      <div className="Container">
+        <FooterForm />
+      </div>
+    </footer>
   );
 };
 
---
+++ src/Components/Footer/FooterForm.jsx
@@ -0,0 +1,31 @@
+import { BsArrowRight } from "react-icons/bs";
+import { Link } from "react-router-dom";
+
+const FooterForm = () => {
+  return (
+    <div className="mx-10 flex flex-col md:flex-row items-center justify-between bg-white px-10 py-8 rounded-lg space-y-4 lg:space-y-0">
+      <div className=" text-title-0">
+        <h1 className="text3xl md:text-2xl lg:text-[27px] font-bold">
+          Subscribe Our Newsletter To <br />
+          Get More Updates
+        </h1>
+      </div>
+      <div className=" ">
+        <form className="flex items-center space-x-5">
+          <input
+            placeholder="Enter Your Email"
+            type="text"
+            className=" px-5 py-[5px] w-fit lg:w-[320px] h-14 text-base bg-[#EEECFE] rounded-md text-[#333] focus:outline-none placeholder:text-[#333]"
+          />
+          <Link to="/">
+            <button className="btn btn-signUp flex items-center justify-center lg:space-x-2">
+              Sign Up <BsArrowRight className="lg:h-5 lg:w-5 ml-1 lg:ml-3" />
+            </button>
+          </Link>
+        </form>
+      </div>
+    </div>
+  );
+};
+
+export default FooterForm;
